6364810a32e7b60eff398628434803cc7da80427
kaminm <kaminibarmanm@gmail.com>
2023-11-25 17:40:55 +0530

    Service registry(Eureka server)is added Department-service jar is created for multiple instances

--- employee-service/src/main/java/upskill/ms/employeeservice/EmployeeServiceApplication.java
+++ employee-service/src/main/java/upskill/ms/employeeservice/EmployeeServiceApplication.java
@@ -4,17 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 @EnableFeignClients
+
 public class EmployeeServiceApplication {
 
 //	@Bean
 //	public RestTemplate restTemplate(){
 //		return  new RestTemplate();
 //	}
+
 
 //	@Bean
 //	public WebClient webClient(){
@@ -24,5 +25,6 @@ public class EmployeeServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeServiceApplication.class, args);
 	}
+
 
 }
--- employee-service/src/main/java/upskill/ms/employeeservice/service/APIClient.java
+++ employee-service/src/main/java/upskill/ms/employeeservice/service/APIClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import upskill.ms.employeeservice.dto.DepartmentDto;
 
-@FeignClient(url = "http://localhost:8080",value = "DEPARTMENT-SERVICE")
+@FeignClient(name="DEPARTMENT-SERVICE")
 public interface APIClient {
     @GetMapping("api/departments/{department-code}")
     DepartmentDto getDepartment(@PathVariable("department-code") String departmentCode) ;
